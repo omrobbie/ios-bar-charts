be7b4597e5800d82516ea13fa5aa202b8a748df7
omrobbie <putu.roby@yahoo.com>
2020-05-27 09:08:24 +0800

    implement charts

--- BarCharts/ViewController.swift
+++ BarCharts/ViewController.swift
@@ -10,7 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var barChartView: BarChartView = {
+        let barChartView = BarChartView()
+        barChartView.frame = view.frame
+        return barChartView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadDummyData()
+    }
+
+    func loadDummyData() {
+        barChartView.dataEntries = [
+            BarEntry(score: 45, title: "Stark"),
+            BarEntry(score: 35, title: "Thor"),
+            BarEntry(score: 55, title: "Evans"),
+            BarEntry(score: 3, title: "Vision"),
+            BarEntry(score: 10, title: "Thanos")
+        ]
+        view.addSubview(barChartView)
     }
 }
